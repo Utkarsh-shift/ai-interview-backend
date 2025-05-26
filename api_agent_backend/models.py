408336c28763fa7640d2b8d45938c1a3c3f22151
Almabay <aditi.goel@almabay.com>
2025-05-26 17:38:30 +0530

    migration for start and end date of interview

--- api_agent_backend/models.py
+++ api_agent_backend/models.py
@@ -55,12 +55,12 @@ class InterviewTranscript(models.Model):
 
 class LipsyncSession(models.Model):
     id = models.AutoField(primary_key=True)
-    lipsync_session_id = models.CharField(max_length=255, unique=True)
     openai_session_id = models.CharField(max_length=255, null=True, blank=True)
     batch_id = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    started_at = models.DateTimeField(null=True, blank=True)
+    ended_at = models.DateTimeField(null=True, blank=True)
     class Meta:
         db_table = 'lipsync_openaiid_batchid'
 
